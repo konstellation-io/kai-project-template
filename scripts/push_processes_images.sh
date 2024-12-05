52cb50300d6afc0a678e354889465b53578d3f7d
Christian <christian.lugo@intelygenz.com>
2024-12-05 15:26:05 +0100

    feat(script): Modify script for get product_name and version from arguments (for make)

--- scripts/push_processes_images.sh
+++ scripts/push_processes_images.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-PRODUCT_NAME="kai-project-template"
-VERSION="v0.1.0"
+# Show help
+function show_help() {
+    echo "Uso: $0 <PRODUCT_NAME> <VERSION>"
+    echo
+    echo "Descripción:"
+    echo "  Script para registrar imágenes de procesos en el registro."
+    echo
+    echo "Argumentos:"
+    echo "  <PRODUCT_NAME>   Nombre del producto."
+    echo "  <VERSION>        Versión del producto."
+    echo
+    echo "Ejemplo:"
+    echo "  $0 kai-project-template v0.1.0"
+    exit 1
+}
+
+# Validate input arguments
+if [ "$#" -ne 2 ]; then
+    echo "Error: Número incorrecto de argumentos."
+    show_help
+fi
+
+# Leer argumentos
+PRODUCT_NAME=$1
+VERSION=$2
 
 SOURCE_PATH="$PWD/src"
 DEMO_WORKFLOW_PATH="$SOURCE_PATH/demo-workflow"
