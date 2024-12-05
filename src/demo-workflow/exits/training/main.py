f2106f26698f92dc1428dcfcc4c08361d9768c63
Christian <christian.lugo@intelygenz.com>
2024-12-05 15:07:19 +0100

    feat(exit): Adapt code for exit main.py

--- src/demo-workflow/exits/training/main.py
+++ src/demo-workflow/exits/training/main.py
@@ -1,21 +1,22 @@
 import asyncio
 from sdk import kai_sdk
-from runner import Runner#, RunnerOptions, LoggerOptions
+from runner import runner as Runner
 
-async def initializer():
-    kai
+
+async def initializer(sdk: kai_sdk):
     pass
 
-async def handler():
+
+async def handler(sdk: kai_sdk):
     pass
 
-async def finalizer():
+
+async def finalizer(sdk: kai_sdk):
     pass
+
 
 async def init():
-    runner = await Runner(
-        options=RunnerOptions(logger_options=LoggerOptions(level="DEBUG"))
-    ).initialize()
+    runner = await Runner().initialize()
     await (
         runner.exit_runner()
         .with_initializer(initializer)
@@ -23,6 +24,7 @@ async def init():
         .with_finalizer(finalizer)
         .run()
     )
+
 
 if __name__ == "__main__":
     loop = asyncio.new_event_loop()
