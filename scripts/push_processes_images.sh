09f27812fd6d94244b25cdb8b0d582457b6bd285
Christian <christian.lugo@intelygenz.com>
2024-12-12 11:40:25 +0100

    feat(script): Update script for ask the image versions

--- scripts/push_processes_images.sh
+++ scripts/push_processes_images.sh
@@ -2,35 +2,37 @@
 
 # Show help
 function show_help() {
-    echo "Uso: $0 <PRODUCT_NAME> <VERSION>"
+    echo "Use: $0 <PRODUCT_NAME>"
     echo
-    echo "Descripción:"
-    echo "  Script para registrar imágenes de procesos en el registro."
+    echo "Description:"
+    echo "  Script for pushing demo workflow processes to the registry."
     echo
-    echo "Argumentos:"
-    echo "  <PRODUCT_NAME>   Nombre del producto."
-    echo "  <VERSION>        Versión del producto."
+    echo "Args:"
+    echo "  <PRODUCT_NAME>   Name of the product."
     echo
-    echo "Ejemplo:"
-    echo "  $0 kai-project-template v0.1.0"
+    echo "Sample:"
+    echo "  $0 kai-project-template"
     exit 1
 }
 
 # Validate input arguments
-if [ "$#" -ne 2 ]; then
-    echo "Error: Número incorrecto de argumentos."
+if [ "$#" -ne 1 ]; then
+    echo "Error: Error in the number of arguments."
     show_help
 fi
 
-# Leer argumentos
+# Read arguments
 PRODUCT_NAME=$1
-VERSION=$2
 
 SOURCE_PATH="$PWD/src"
 DEMO_WORKFLOW_PATH="$SOURCE_PATH/demo-workflow"
 
 echo "Pushing demo workflow processes to registry..."
-kli process-registry register trigger trigger --dockerfile "$DEMO_WORKFLOW_PATH/triggers/rest-trigger/Dockerfile" --product $PRODUCT_NAME --src "$DEMO_WORKFLOW_PATH/triggers/rest-trigger" --version $VERSION
-kli process-registry register exit exit --dockerfile "$DEMO_WORKFLOW_PATH/exits/training/Dockerfile" --product $PRODUCT_NAME --src "$DEMO_WORKFLOW_PATH/exits/training" --version $VERSION
+echo "Enter the version of the trigger: "
+read TRIGGER_VERSION
+kli process-registry register trigger trigger --dockerfile "$DEMO_WORKFLOW_PATH/triggers/rest-trigger/Dockerfile" --product $PRODUCT_NAME --src "$DEMO_WORKFLOW_PATH/triggers/rest-trigger" --version $TRIGGER_VERSION
+echo "Enter the version of the exit: "
+read EXIT_VERSION
+kli process-registry register exit exit --dockerfile "$DEMO_WORKFLOW_PATH/exits/training/Dockerfile" --product $PRODUCT_NAME --src "$DEMO_WORKFLOW_PATH/exits/training" --version $EXIT_VERSION
 
 echo "Done"
